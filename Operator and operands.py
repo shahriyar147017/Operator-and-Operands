02b45942b0958c10d37cb84d823458e214edf201
shahriyar147017 <72671139+shahriyar147017@users.noreply.github.com>
2020-10-11 03:14:21 +0600

    Add files via upload

---
+++ Operator and operands.py
@@ -0,0 +1,13 @@
+#Arithmetic Operators (addition, substraction, multiplication, division, Modulus, Exponent, Floor Division)
+a = 10
+b = 20
+add = a+b
+sub = a-b
+mul= a*b
+div= a/b
+print ("Addition =", add, "\nSubtraction =", sub, "\nMultiplication =", mul, "\nDivision =", div)
+print ("Modulus =", (b%a), "\nExponent (type 1)=", (b**a), "Exponent (type 2)=", pow(b,a), "\nFloor Division =", (9//2))
+
+#Comparison Operators
+print (a==b, "\n",a!=b, "\n",(a>b), "\n",(a<b),"\n",(a>=b), "\n",(a<=b))
+
